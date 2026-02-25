d5d46df7845a210043d2ddc601563d13d72e2371
Maël Auzenet <mauzenet@gmail.com>
2026-02-25 16:59:26 +0100

    v2 all

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/REPO_NAME/', // ← remplace REPO_NAME par le nom exact de ton dépôt GitHub
   server: {
     port: 3000
   }
